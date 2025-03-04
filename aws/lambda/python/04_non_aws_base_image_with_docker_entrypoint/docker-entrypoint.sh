0e2a75002bd45f782f1ffe6b04348ece1c0ff0aa
Pete Lindgren <>
2025-03-04 08:07:14 -0500

    #34 refactor entry_script.sh as new set_aws_lambda_runtime shell script function

--- aws/lambda/python/04_non_aws_base_image_with_docker_entrypoint/docker-entrypoint.sh
+++ aws/lambda/python/04_non_aws_base_image_with_docker_entrypoint/docker-entrypoint.sh
@@ -8,10 +8,23 @@ help() {
     echo "run_lambda replaces the AWS Tutorial ENTRYPOINT and CMD"
 }
 
+set_aws_lambda_runtime()
+{
+    if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
+        RUNTIME="/usr/local/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric"
+    else
+        # Set default runtime
+        RUNTIME="/usr/local/bin/python -m awslambdaric"
+    fi
+}
+
+set_aws_lambda_runtime
+# echo "exec lambda with runtime: '${RUNTIME}'"
+
 case "$1" in
     run_lambda)
         shift
-        /usr/local/bin/python -m awslambdaric lambda_function.handler
+        ${RUNTIME} lambda_function.handler
         ;;
     help)
         shift
