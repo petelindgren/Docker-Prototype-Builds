f3d4856d1106e70c68c2e19336b661d359317e3b
Pete Lindgren <>
2026-03-15 12:03:13 -0400

    issue #52 04-demo: update docker-compose alembic db

--- python/04_fastapi_alembic_demo/database.py
+++ python/04_fastapi_alembic_demo/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:passw0rd@localhost/fastapi_demo"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:passw0rd@localhost/fastapidemo"
 # For SQLite: "sqlite:///./test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
