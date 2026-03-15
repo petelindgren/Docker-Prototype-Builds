cdc2f84dc6573dd0cf153e27e17c2a65d0c9bc37
Pete Lindgren <>
2026-03-15 11:35:26 -0400

    issue #52 04-demo: prepare alembic for migrations

--- python/04_fastapi_alembic_demo/alembic/env.py
+++ python/04_fastapi_alembic_demo/alembic/env.py
@@ -1,9 +1,15 @@
+import os
+import sys
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+from models import Base
+
+# Add the project root to the Python path
+sys.path.append(os.getcwd())
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- python/04_fastapi_alembic_demo/database.py
+++ python/04_fastapi_alembic_demo/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
+SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:passw0rd@localhost/fastapi_demo"
 # For SQLite: "sqlite:///./test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
